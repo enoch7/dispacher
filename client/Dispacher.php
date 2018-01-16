1be78ca5c694b61196383807ccf6ccce75d11e22
enoch7 <xingbenyong@gmail.com>
2018-01-16 23:06:40 +0800

    moniter

--- client/Dispacher.php
+++ client/Dispacher.php
@@ -44,11 +44,43 @@ class Dispacher
 	public  function getSequence(string $key,int $num = 1)
 	{
 		try {
-			$redis = $this->getRedisConnection();
-			if ($redis->exists($key)) {
-				$number = 0;
+			$number = $this->getSequenceValue($key);
+			if ($number == 0) {
+				$this->updateSequence($key);
+				$number = $this->getSequenceValue($key);
+			} 
+			return $number;
+		} catch (\Exception $e) {
+			//do something
+			throw $e;
+		}
+	}
 
-				$luaScript = <<<LUA
+	public function updateSequence(string $key)
+	{
+		if ($redis->set("lock:set:".$key, 1, ['NX', 'EX'=>10])) {
+			$dbconn = $this->getDbConnection();
+			$sql = "select max,step from sequence where name = '{$key}' limit 1";
+			$result = $dbconn->query($sql);
+			$row = $result->fetch_array(MYSQLI_ASSOC);
+			if ($row['max']) {
+				$sql = "update sequence set max = last_insert_id(max + step) where name = '{$key}'";
+				if ($dbconn->query($sql)) {
+					$redis->hMSet($key,['current'=>$row['max'],'max'=>($row['max'] + $row['step'])]);
+				}		
+			}
+			$redis->del("lock:set:".$key);
+		} else {
+			sleep(1);
+		}
+	}
+
+	public function getSequenceValue($key)
+	{
+		$number = 0;
+		$redis = $this->getRedisConnection();
+		if ($redis->exists($key)) {
+			$luaScript = <<<LUA
 local current = redis.call("HGET", KEYS[1], "current")
 local max = redis.call("HGET", KEYS[1], "max")
 if max > current then
@@ -58,30 +90,10 @@ else
     return 0
 end
 LUA;
-				$number = $redis->eval($luaScript,[$key],1);
-			} else {
-				$number = 0;
-				if ($redis->set("lock:set:".$key, 1, ['NX', 'EX'=>10])) {
-					$dbconn = $this->getDbConnection();
-					$sql = "select max,step from sequence where name = '{$key}' limit 1";
-					$result = $dbconn->query($sql);
-					$row = $result->fetch_array(MYSQLI_ASSOC);
-					if ($row['max']) {
-						$sql = "update sequence set max = last_insert_id(max + step) where name = '{$key}'";
-						if ($dbconn->query($sql)) {
-							$redis->hset($key, 'current', $row['max']);
-							$redis->hset($key, 'max', $row['max']+ $row['step']);
-							$number = $row['max'] + 1;
-						}		
-					}
-					$redis->del("lock:set:".$key);
-				}
-			}
-			return $number;
-		} catch (\Exception $e) {
-			//do something
-			throw $e;
+			$number = $redis->eval($luaScript,[$key],1);		
+
 		}
+		return $number;
 	}
 }
 
--- daemon/Moniter.php
+++ daemon/Moniter.php
@@ -5,10 +5,13 @@
 class Moniter
 {
 	private $redis;
+	private $dbconn;
+
+	private $lastNumber = 0;
 
 	public function __construct()
 	{
-
+		$this->init();
 	}
 
 	public function init()
@@ -16,12 +19,79 @@ class Moniter
 
 	}
 
-	public function start()
+	public function getRedisConnection()
 	{
-		while (true) {
+		if ($this->redis) {
+			return $this->redis;
+		}
+		$redis = new \Redis();
+		$redis->connect($this->config['redis']['hostname'], $this->config['redis']['port']);
 
+		if ($redis->ping()) {
+			$this->redis = $redis;
+			return $this->redis;
+		} else {
+			throw new \RedisException("redis connnect fail");
 		}
 
 	}
+
+	public function getDbConnection()
+	{
+		if ($this->dbconn) {
+			return $this->dbconn;
+		}
+
+		$conn = new mysqli('127.0.0.1','root','root','test');
+		$this->dbconn = $conn;
+		return $this->dbconn;
+
+	}
+
+	public function start()
+	{
+		$sleepTime = 10;
+		while (true) {
+			try {
+				$key = '20180110';
+				$redis = $this->getRedisConnection();
+				$dbconn = $this->getDbConnection();
+				$sql = "select max,step from sequence where name = '{$key}' limit 1";
+				$result = $dbconn->query($sql);
+				$persistentData = $result->fetch_array(MYSQLI_ASSOC);
+				$step = $persistentData['step'];
+
+				$currentStat = $redis->hGetAll($key);
+				if ($currentStat['current'] - $this->lastNumber > $step) {
+					$step = $currentStat['current'] - $this->lastNumber;
+					$sql = "update sequence set step = " . $step . " where name = '{$key}' ";
+					$dbconn->query($sql);
+				}
+
+				if ($currentStat['max'] - $currentStat['current'] < $step * (60/$sleepTime) * 10) {
+					if ($redis->set("lock:set:".$key, 1, ['NX', 'EX'=>10])) {
+						$sql = "update sequence set max = last_insert_id(max + step) where name = '{$key}'";
+						if ($dbconn->query($sql)) {
+							$redis->hSet($key, 'max', ($currentStat['max'] + $step * (60/$sleepTime) * 10));
+						}		
+						$redis->del("lock:set:".$key);
+					}	
+				}
+
+			} catch (\Exception $e) {
+				if ($e instanceof \RedisException) {
+					$this->redis = null;
+				} else if ($e instanceof \PDOException) {
+					$this->dbconn = null;
+				} else {
+					//log excption;
+				}
+			}
+			sleep($sleepTime);
+		}	
+		
+	}
+
+
 	
 }
